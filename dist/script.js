3eca1b5f345eb4c87f0b9b15e24602a0eba83ef1
gonzolog <benwelly4@gmail.com>
2023-11-13 11:04:50 +0000

    Finished responsive design

--- dist/script.js
+++ dist/script.js
@@ -1,33 +1,13 @@
-// Function that checks screen width
-// Categorise into (mobile, tablet, etc)
-function checkScreenWidth() {
-    let screenWidth = window.innerWidth;
-    if (screenWidth <= 480) {
-        return 'mobile';
-    } else if (screenWidth >= 481 && screenWidth <= 768) {
-        return 'tablet';
-    } else if (screenWidth >= 769 && screenWidth <= 1024) {
-        return 'laptop';
-    } else if (screenWidth >= 1025 && screenWidth <= 1200) {
-        return 'desktop';
-    } else if (screenWidth >= 1201) {
-        return 'desktop-large';
-    }
-};
 // Fn to shorten intro message for mobile devices
 function updateIntroMessage() {
     let introMessage = document.querySelector('.intro-message');
-    let deviceType = checkScreenWidth();
     let h1Element = introMessage.querySelector('h1');
     let h2Elements = introMessage.querySelectorAll('h2');
     var nav = introMessage.querySelector('.transparent-overlay');
     var flexContainer = introMessage.querySelector('.flex.center.column');
+    var screenWidth = window.innerWidth;
 
-    if (deviceType === 'mobile') {
-        //Remove h1 element '<Hello!/>'
-        if (h1Element) {
-            h1Element.hidden = true
-        }
+    if (screenWidth <= 538) {
         //Shorten intro message 
         h2Elements.forEach(function(h2) {
             if (h2.textContent.includes('Ben Wellington')) {
@@ -36,19 +16,8 @@ function updateIntroMessage() {
                 h2.textContent = 'Full-Stack Developer';
             }
         });
-        // Repostions nav bar to be ontop of the intro message
-        flexContainer.insertBefore(nav, flexContainer.firstChild);
-        introMessage.querySelector('.social-media-icons').classList.remove("hidden")
-        // Remove hidden property for social media icons
-
-
-        // Re align social media icons with intro message
     } else {
         // Revert changes made for mobile
-        if (h1Element) {
-            h1Element.hidden = false;
-        }
-
         h2Elements.forEach(function(h2) {
             if (h2.textContent.includes('Ben Wellington')) {
                 h2.innerHTML = 'My name is <span class="apricot">Ben Wellington,</span>';
@@ -56,13 +25,13 @@ function updateIntroMessage() {
                 h2.innerHTML = 'and I am a <span class="apricot">full-stack developer.</span>';
             }
         });
-        introMessage.querySelector('.social-media-icons').classList.add("hidden")
+    }
+    if (screenWidth <= 1000) {
+        // Repostions nav bar to be ontop of the intro message
+        flexContainer.insertBefore(nav, flexContainer.firstChild);
+    } else {
         flexContainer.insertBefore(nav, flexContainer.lastChild);
     }
 }
-
-
 window.addEventListener('load', updateIntroMessage);
 window.addEventListener('resize', updateIntroMessage);
-window.addEventListener('load', checkScreenWidth);
-window.addEventListener('resize', checkScreenWidth);
