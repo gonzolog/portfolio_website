2b3f2122b864380c1c92d002f39ab16868751166
gonzolog <benwelly4@gmail.com>
2023-11-14 22:11:01 +0000

    commit before attempting hamburger menu

--- dist/script.js
+++ dist/script.js
@@ -12,8 +12,8 @@ function updateIntroMessage() {
         h2Elements.forEach(function(h2) {
             if (h2.textContent.includes('Ben Wellington')) {
                 h2.textContent = 'Ben Wellington';
-            } else if (h2.textContent.includes('full-stack developer')) {
-                h2.textContent = 'Full-Stack Developer';
+            } else if (h2.textContent.includes('web developer')) {
+                h2.textContent = 'Web Developer';
             }
         });
         flexContainer.insertBefore(nav, flexContainer.firstChild);
@@ -22,8 +22,8 @@ function updateIntroMessage() {
         h2Elements.forEach(function(h2) {
             if (h2.textContent.includes('Ben Wellington')) {
                 h2.innerHTML = 'My name is <span class="apricot">Ben Wellington,</span>';
-            } else if (h2.textContent.includes('Full-Stack Developer')) {
-                h2.innerHTML = 'and I am a <span class="apricot">full-stack developer.</span>';
+            } else if (h2.textContent.includes('Web Developer')) {
+                h2.innerHTML = 'and I am a <span class="apricot">web developer.</span>';
             }
         });
         flexContainer.insertBefore(nav, flexContainer.lastChild);
